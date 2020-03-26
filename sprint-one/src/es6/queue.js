58a3f6782631ca1aaa9666aed43d2ed8836a8ce2
Steve Marquez <steveantonio00@gmail.com>
2020-03-25 17:36:50 -0700

    Complete queue in es6;

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+
+  dequeue() {
+    var keys = Object.keys(this.storage);
+    var first = keys[0];
+    var firstProp = this.storage[first];
+
+    delete this.storage[first];
+    return firstProp;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
