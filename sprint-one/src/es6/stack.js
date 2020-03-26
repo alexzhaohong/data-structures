eb0e514d17225dec4fe24fd31bb62873afee5bba
Steve Marquez <steveantonio00@gmail.com>
2020-03-25 17:37:12 -0700

    Complete stacks in es6

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
   }
 
+  push(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+  pop() {
+    var keys = Object.keys(this.storage);
+    var last = keys[keys.length - 1];
+    var lastProp = this.storage[last];
+
+    delete this.storage[last];
+    return lastProp;
+  }
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
