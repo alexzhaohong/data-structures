e59d21847fc701614921996fe76ac5f1a3d133d3
Steve Marquez <steveantonio00@gmail.com>
2020-03-26 22:09:53 -0700

    Thinking about tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -16,8 +16,18 @@ treeMethods.addChild = function(value) {
 treeMethods.contains = function(target) {
 };
 
-
-
 /*
- * Complexity: What is the time complexity of the above functions?
- */
+Tree() constructor
+*****Justification*****
+Hierarchical recursive data structure great for family trees
+
+*****Specification*****
+I:NA
+O:Tree Object
+  Side Effects: NA
+  Edge case: NA
+
+*****Explanation*****
+A tree with a root node that has any number of chilren nodes, arrays containing a number of subtrees. For methods, it takes any value, sets that as the target of a node, and adds that node as a child of the tree. O(n)
+contains() takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node O(n)
+*/
