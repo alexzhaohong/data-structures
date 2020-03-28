4216ca11e95886d28af64b79c4f4b4111fd85525
Steve Marquez <steveantonio00@gmail.com>
2020-03-27 19:26:39 -0700

    Implement tree data structure

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,3 +1,4 @@
+// functional with shared method pattern
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
@@ -22,28 +23,102 @@ treeMethods.addChild = function(value) {
   //console.log(this.children, 'Children');
 };
 
+
+
+
+//contains() takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node O(n)
+//input target value
+//output boolean
 treeMethods.contains = function(target) {
-  // check if the first value in newTree.value is the target that we are looking for
-  this.value === target ? true : false;
 
-  // since children is an array we can iterate over length
-  for (var i = 0; i < this.children.length; i++) {
-    // check all of them
-    var child = this.children[i];
-
-    // if it doesnt find it return false;
+// make var found, set to false
+  var found = false;
+// check node at right now. if node value === target value
+//  return found = true
+  if ( this.value === target ) {
+    found = true;
+    return found;
   }
-  return false;
+
+    // else if node.children is undefined
+    // return found = false
+  if (this.children.length === 0) {
+    found = false;
+    return found;
+  }
+
+    // else if node.children is defined
+    // access all the children of node, iterate (newTree.children = [];) for i++
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      // method contains on current element.this.children[i]
+      // store this.contains(children[i]) in found
+      var bool = this.children[i].contains(target);
+      // if found === true )
+      if (bool === true) {
+        return found = bool;
+      }
+    }
+  }
+
+  return found;
+
 };
 
-var test = Tree()
-test.addChild(2);
+
+var test = Tree(2);
 test.addChild(12);
 test.addChild(23);
 test.addChild(34);
 test.contains(1);
 test.contains(2);
 test.contains(12);
+
+// check node at right now. if node value === target value
+//  return found = true
+// else if node.children is undefined
+  // return found = false
+
+
+// else if node.children is defined
+  // access all the children of node, iterate (newTree.children = [];) for i++
+    // method contains on current element.this.children[i]
+    // store this.contains(children[i]) in found
+    //if found === true )
+      // break;
+
+// return found
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var found = false;
+
+  // // check if the first value in newTree.value is the target that we are looking for
+  // found = (this.value === target) ? true : false;
+  // // since children is an array we can iterate over length
+  // for (var i = 0; i < this.children.length; i++) {
+  //   // check all of them
+  //   var child = this.children[i];
+
+  //   if (this.contains(child)) {
+  //     found = true;
+  //   }
+
+  //   // if it doesnt find it return false;
+  // }
+  // return found;
+
+
 
 /*
 Tree() constructor
