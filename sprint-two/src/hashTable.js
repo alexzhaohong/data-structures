6e73c46453e8dc91c29b19e334a3affeb9e3f3a7
Steve Marquez <steveantonio00@gmail.com>
2020-03-28 14:42:06 -0700

    WIP for hash table data structure

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -6,7 +6,9 @@ var HashTable = function() {
 };
 
 HashTable.prototype.insert = function(k, v) {
+// figure out index of array by running it with getIndexBellowMaxForKey(Hash funciton- helper function)
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // check if storage contains that index
 };
 
 HashTable.prototype.retrieve = function(k) {
